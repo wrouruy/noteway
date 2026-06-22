cf70cd91e84a35abda3c4f34c423f56ec47a13a2
wrouruy <wroyruy@gmail.com>
2026-06-22 08:53:30 +0300

    add PUT /api/user for updating username

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -36,3 +36,48 @@ export async function GET() {
         client.release();
     }
 }
+
+export async function PUT (req: NextRequest) {
+    const cookieStore = await cookies();
+    const sessionToken = cookieStore.get('session_token')?.value;
+
+    if (!sessionToken || !validate(sessionToken))
+        return NextResponse.json(
+            { ok: false, user: null, message: 'session token is invalid' },
+            { status: 400 });
+    
+    const client = await pool.connect();
+    try {
+        const { username } = await req.json();
+
+        const busy = await client.query(`
+            SELECT * FROM users
+            WHERE name = $1`, [username]);
+
+        if (busy.rows.length > 0) {
+            await client.query(`ROLLBACK`);
+            return NextResponse.json(
+                { ok: false, user: null, message: 'username is busy' },
+                { status: 400 });
+        }
+
+        const user = await userBySession(client, sessionToken);
+
+        const res = await client.query(`
+            UPDATE users
+            SET name = $1
+            WHERE id = $2
+            RETURNING *`, [username, user.id]);
+        
+        return NextResponse.json(
+            { ok: true, user: res.rows[0] });
+
+    } catch(err: any) {
+        await client.query(`ROLLBACK`);
+        return NextResponse.json(
+            { ok: false, message: err.message || 'Iternal server error' },
+            { status: 500 });
+    } finally {
+        client.release();
+    }
+}
